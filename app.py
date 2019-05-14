23bc79415e773a5ebf899f7fbb7d7c9218c13129
klanmiko <eternity01033@gmail.com>
2019-05-13 20:45:54 -0700

    add some graphs

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_file, request
 import pyogg
 import audioop
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,7 +15,14 @@ def audio():
     speech = request.files['speech']
     speech.save('./speech.ogg')
     opus = pyogg.OpusFile('./speech.ogg')
-    print(opus.buffer_length)
     cross = audioop.cross(opus.buffer, 1)
+    print(opus.buffer_length)
+    buffer = np.ctypeslib.as_array(opus.buffer, shape=(1000,))
+    print(buffer)
+    f, t, Sxx = signal.spectrogram(buffer, 100)
+    plt.pcolormesh(t, np.fft.fftshift(f), np.fft.fftshift(Sxx, axes=0))
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
     print(cross)
     return app.make_response('')
