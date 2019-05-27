b1af0fe504a9293a6b6d9a1d3448f9aec99168d1
The-Boogeyman <aoblebea@ucdavis.edu>
2019-05-26 23:14:16 -0700

    All borken python libraries can be replaced by command line utilities

--- stitch_attack.py
+++ stitch_attack.py
@@ -3,7 +3,10 @@ import scipy
 import scipy.io.wavfile
 import os
 
+text_as_wav = lambda text, frequency : print("echo " + text + " | text2wave -scale 2 -o __tempfile.wav; sox -S __tempfile.wav __tempfile2.wav rate -L -s " + str(frequency))
+
 os.system("echo hello")
+text_as_wav("hello there", 44100)
 
 rate1, data1 = scipy.io.wavfile.read("Matlab_Attempt/hal.wav")
 rate2, data2 = scipy.io.wavfile.read("Matlab_Attempt/bootleg.wav")
